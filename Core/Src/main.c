4579808c5a871de225e8fbcb45039aad11c97a40
ErrorInever <errorinever@gmail.com>
2026-01-31 21:53:29 +0300

    refactoring

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -7,7 +7,6 @@ LD2 user: port PA5 (pin 21)
 // RCC enable ports
 #define GPIOA_EN (1 << 0)
 #define GPIOC_EN (1 << 2)
-
 // Pin mode
 // PA5 reset
 #define PA5_R (3 << (2 * 5))
@@ -21,7 +20,6 @@ LD2 user: port PA5 (pin 21)
 #define PC13_EN_P (1 << (2 * 13))
 
 // GPIO register addresses + mode offset
-//
 // GPIOA addresses: 0x4002 0000 - 0x4002 03FF
 // ODR register offset for all: 0x14
 // MODER register offset: 0x00
@@ -33,20 +31,14 @@ LD2 user: port PA5 (pin 21)
 #define GPIOC_IDR_REG_ADDR (volatile uint32_t *)(0x40020800UL + 0x10UL)
 // PUPDR register offset: 0x0C
 #define GPIOC_PUPDR_REG_ADDR (volatile uint32_t *)(0x40020800UL + 0x0CUL)
-
-/*
-
-
-RCC addresses: 0x4002 3800 - 0x4002 3BFF
-AHB1ENR register offset: 0x30
-*/
+/*RCC addresses: 0x4002 3800 - 0x4002 3BFF
+AHB1ENR register offset: 0x30*/
 #define RCC_AHB1ENR_ADDRS (volatile uint32_t *)(0x40023800UL + 0x30UL)
 
 
 int main(void) {
   // RCC on GPIOA and GPIOC
   *RCC_AHB1ENR_ADDRS |= GPIOA_EN | GPIOC_EN;
-  
   // PA5 reset and set pin mode "output" in register GPIOA_MODER
   *GPIOA_MODER_REG_ADDR &= ~PA5_R;
   *GPIOA_MODER_REG_ADDR |= PA5_EN;
@@ -56,7 +48,6 @@ int main(void) {
   // PC13 reset and set pull-up mode in register GPIOC_PUPDR
   *GPIOC_PUPDR_REG_ADDR &= ~PC13_R; 
   *GPIOC_PUPDR_REG_ADDR |= PC13_EN_P;
-
 
   while (1) {
     // PC13 button is pressed
