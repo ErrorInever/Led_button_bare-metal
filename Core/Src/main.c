88ddb3f162e0a5f766e9181f2f31ceb9ec4f5a94
ErrorInever <errorinever@gmail.com>
2026-01-31 17:33:23 +0300

    start project

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,29 +1,79 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2026 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
 #include "main.h"
+#include <stdint.h>
+/*
+B1 user:  port PC13 (pin 2) 
+LD2 user: port PA5 (pin 21)
+*/
+// RCC enable ports
+#define GPIOA_EN (1 << 0)
+#define GPIOC_EN (1 << 2)
+
+// Pin mode
+// PA5 reset
+#define PA5_R (3 << (2 * 5))
+// PA5 enable
+#define PA5_EN (1 << (2 * 5))
+// PC13 MODER reset
+#define PC13_R (3 << (2 * 13))
+// PC13 enable MODER 
+#define PC13_EN (0 << (2 * 13))
+// PC13 enable pull-up
+#define PC13_EN_P (1 << (2 * 13))
+
+// GPIO register addresses + mode offset
+//
+// GPIOA addresses: 0x4002 0000 - 0x4002 03FF
+// ODR register offset for all: 0x14
+// MODER register offset: 0x00
+#define GPIOA_MODER_REG_ADDR (volatile uint32_t *)(0x40020000UL + 0x00UL)
+#define GPIOA_ODR_REG_ADDR (volatile uint32_t *)(0x40020000UL + 0x14UL)
+// GPIOC addresses: 0x4002 0800 - 0x4002 0BFF
+#define GPIOx_MODER_REG_ADDR (volatile uint32_t *)(0x40020800UL + 0x00UL)
+// IDR register offset: 0x10
+#define GPIOC_IDR_REG_ADDR (volatile uint32_t *)(0x40020800UL + 0x10UL)
+// PUPDR register offset: 0x0C
+#define GPIOC_PUPDR_REG_ADDR (volatile uint32_t *)(0x40020800UL + 0x0CUL)
+
+/*
+
+
+RCC addresses: 0x4002 3800 - 0x4002 3BFF
+AHB1ENR register offset: 0x30
+*/
+#define RCC_AHB1ENR_ADDRS (volatile uint32_t *)(0x40023800UL + 0x30UL)
 
 
 int main(void) {
+  // RCC on GPIOA and GPIOC
+  *RCC_AHB1ENR_ADDRS |= GPIOA_EN | GPIOC_EN;
+  
+  // PA5 reset and set pin mode "output" in register GPIOA_MODER
+  *GPIOA_MODER_REG_ADDR &= ~PA5_R;
+  *GPIOA_MODER_REG_ADDR |= PA5_EN;
+  // PC13 reset and set pin mode "input" in register GPIOC_MODER
+  *GPIOx_MODER_REG_ADDR &= ~PC13_R;
+  *GPIOx_MODER_REG_ADDR |= PC13_EN;
+  // PC13 reset and set pull-up mode in register GPIOC_PUPDR
+  *GPIOC_PUPDR_REG_ADDR &= ~PC13_R; 
+  *GPIOC_PUPDR_REG_ADDR |= PC13_EN_P;
+
 
   while (1) {
-
+    // PC13 button is pressed
+    if(!(*GPIOC_IDR_REG_ADDR & (1 << 13))) {
+      // debouncing delay
+      for(volatile uint32_t i = 0; i < 100000; i++);
+      // double-check is pressed 
+      if(!(*GPIOC_IDR_REG_ADDR & (1 << 13))) {
+        // led on
+        *GPIOA_ODR_REG_ADDR |= (1 << 5);
+      } 
+    }
+    else {
+        // led off
+        *GPIOA_ODR_REG_ADDR &= ~(1 << 5);
+    }
   }
-
 }
 
 void Error_Handler(void)
